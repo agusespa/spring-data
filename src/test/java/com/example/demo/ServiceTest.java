f5bc1e3dc199eebf4c400dbd6cfc86401d8fb902
Agustin Espana <agustinespania@gmail.com>
2022-09-01 12:39:22 +0200

    Update ServiceTest.java

--- src/test/java/com/example/demo/ServiceTest.java
+++ src/test/java/com/example/demo/ServiceTest.java
@@ -1,17 +1,36 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class ServiceTest {
 
-    @Test
-    void fetchItems() {
-    }
+    @Mock
+    Repo repo;
+
+    @InjectMocks
+    Service service;
 
     @Test
-    void searchItems() {
+    void shouldReturnSearchItemsContainingDesk() {
+        List mockedList = List.of(
+                new Item("Table"),
+                new Item("Desk"),
+                new Item("Computer Desk"));
+
+        when(repo.findByNameContainingIgnoreCase("desk")).thenReturn(mockedList);
+
+        List fetchedList = service.searchItems("desk");
+
+        assertTrue(fetchedList.equals(mockedList));
     }
 
 }
