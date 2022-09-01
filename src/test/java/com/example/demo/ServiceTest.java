f0a5b63e2d02e9027a3704b0f421a203fb1b5342
Agustin Espana <agustinespania@gmail.com>
2022-09-01 12:26:05 +0200

    Create ServiceTest.java

---
+++ src/test/java/com/example/demo/ServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    void fetchItems() {
+    }
+
+    @Test
+    void searchItems() {
+    }
+
+}
