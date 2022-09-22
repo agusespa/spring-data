4c1ee6523a2b4768eeda5ede9edd6b0ff084c213
Agustin Espana <agustinespania@gmail.com>
2022-09-22 18:00:54 +0200

    Update ServicesTest.java

--- src/test/java/com/example/demo/service/ServicesTest.java
+++ src/test/java/com/example/demo/service/ServicesTest.java
@@ -16,7 +16,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ServiceTest {
+class ServicesTest {
 
     @Mock
     Repo repo;
