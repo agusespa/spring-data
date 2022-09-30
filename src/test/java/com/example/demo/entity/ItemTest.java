042f079b52cbd5b2e140d241ecdb4853a0670bd9
Agustin Espana <agustinespania@gmail.com>
2022-09-30 11:33:18 +0200

    Update ItemTest.java

--- src/test/java/com/example/demo/entity/ItemTest.java
+++ src/test/java/com/example/demo/entity/ItemTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -11,16 +14,19 @@ class ItemTest {
 
     private Item referenceItem;
 
+    private Item testedItem;
+
     @BeforeAll
     void setUp() {
         referenceItem = new Item("Chair");
         referenceItem.setId("B_58");
+
+        testedItem = new Item("Chair");
+        testedItem.setId("B_58");
     }
 
     @Test
     void equalsShouldReturnTrueForEqualObjects() {
-        Item testedItem = new Item("Chair");
-        testedItem.setId("B_58");
         assertTrue(referenceItem.equals(testedItem));
         assertTrue(testedItem.equals(referenceItem));
         assertTrue(testedItem.equals(testedItem));
@@ -28,13 +34,14 @@ class ItemTest {
 
     @Test
     void equalsShouldReturnFalseForDifferentObjects() {
-        Item testedItem = new Item("Chairs");
-        testedItem.setId("B_54");
         assertFalse(referenceItem.equals(testedItem));
         assertFalse(testedItem.equals(referenceItem));
     }
 
     @Test
     void testHashCode() {
+        Set<Item> set = new HashSet<>();
+        set.add(referenceItem);
+        assertFalse(set.add(testedItem));
     }
 }
